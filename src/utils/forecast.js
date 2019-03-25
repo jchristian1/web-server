053faad9143e0e02c5e9cf7287d7dbcdd34f2be7
jchristian1 <jchristian_1@hotmail.com>
2019-03-25 01:23:55 -0400

    Showing highest and lowest temperature new features on weather page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. Lowest temperature for today should be '+ body.daily.data[0].temperatureLow  +' and highest termperature would be '+ body.daily.data[0].temperatureHigh +'. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
